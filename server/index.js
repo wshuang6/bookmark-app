527a0956ac6ea4e393306cbe847e32b33c0f1bde
HexisHacks <franklin.a.carvajal@gmail.com>
2017-04-17 23:52:51 -0400

    forking all Bookmarks and Folders Endpoints

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const path = require('path');
 const express = require('express');
 const {DEV, PROD} = require('./config');
 const knex = require('knex')(DEV);
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
 
 const app = express();
 
@@ -15,6 +17,182 @@ app.get('/users', (req, res) => {
             res.json(results);
         })
 })
+
+// FOLDERS GET ENDPOINTS
+
+app.get('/folders', (request, res) => {
+    knex('folders')
+        .select(['foldername', 'userid'])
+        .then(results => {
+            res.json(results);
+        })
+        .catch((error) => {
+            console.error('ERROR:', error.message || error);
+            res.status(500);
+    })
+})
+
+app.get('/', (request, res) => {
+    knex('bookmarks')
+        .select(['url', 'title', 'notes', 'folderid', 'image', 'bookmarkid', 'userid'])
+        .then(results => {
+            res.json(results);
+        })
+        .catch((error) => {
+            console.error('ERROR:', error.message || error);
+            res.status(500);
+    })
+})
+
+
+app.get('/:id', (req, res) => {
+    knex('bookmarks')
+        .select(['url', 'title', 'notes', 'folderid', 'image', 'bookmarkid', 'userid'])
+        .where('userid', req.params.id)
+        .then(results => {
+            res.json(results);
+        })
+        .catch((error) => {
+            console.error('ERROR:', error.message || error);
+            res.status(500);
+    })
+})
+
+// BOOKMARK POST AND PATCH ENDPOINTS
+
+
+app.post('/', jsonParser, (req, res) => {
+    const { url, title, notes, folderid, image, bookmarkid, userid } = req.body;
+	if (url === '' || url === ' '|| typeof url === 'undefined') {
+		return res.status(404).json({message: 'Bad request: enter valid bookmark url'});
+	} 
+    knex('bookmarks').insert({
+    url: url,
+  	title: title,
+	notes: notes,
+  	folderid: folderid,
+  	image: image,
+    userid: userid
+  })
+	.returning(['url', 'title', 'notes', 'folderid', 'image', 'bookmarkid', 'userid'])
+	.then(result => {
+		return res.status(201).json(result);
+	})
+	.catch(error => { 
+		console.log(error);
+		return res.status(500).json({message: "Internal server error"});  
+	});
+})
+
+
+
+// BOOKMARK PUT ENDPOINT
+
+app.patch('/:id', jsonParser, (req, res) => {
+     knex('bookmarks')
+     .where('bookmarkid', req.params.id)
+     .update(req.body)
+     .then(result => {
+     return res.status(202).send('patch was successful');
+   })
+	 .catch(err => {
+   	 console.log(err);
+		 return res.status(500).json({message: "Internal server error"}); 
+   });
+})
+
+// BOOKMARK DELETE ENDPOINT
+
+app.delete('/:id', (req, res) => {
+	knex.del()
+    .where('bookmarkid',req.params.id)
+    .from('bookmarks')
+   .then(result => {
+     return res.status(202).send('delete was successful');
+   })
+	 .catch(err => {
+   	 console.log(err);
+		 return res.status(500).json({message: "Internal server error"}); 
+   });
+})
+
+
+
+
+// FOLDERS SPECIFIC GET ENDPOINT
+
+app.get('/folders/:id', (req, res) => {
+    knex('folders')
+        .select(['foldername', 'userid'])
+        .where('folderid', req.params.id)
+        .then(results => {
+            res.json(results);
+        })
+        .catch((error) => {
+            console.error('ERROR:', error.message || error);
+            res.status(500);
+    })
+})
+
+// FOLDERS POST ENDPOINT
+
+app.post('/folders', jsonParser, (req, res) => {
+    const { foldername, userid } = req.body;
+	if (foldername === '' || foldername === ' '|| typeof foldername === 'undefined') {
+		return res.status(404).json({message: 'Bad request: enter valid foldername'});
+	} 
+    knex('folders').insert({
+  	foldername: foldername,
+    userid: userid
+  })
+	.returning(['foldername', 'userid'])
+	.then(result => {
+		return res.status(201).json(result);
+	})
+	.catch(error => { 
+		console.log(error);
+		return res.status(500).json({message: "Internal server error"});  
+	});
+})
+
+// // FOLDER PATCH ENDPOINT
+
+app.patch('/folders/:id', jsonParser, (req, res) => {
+    knex('folders')
+     .where('folderid', req.params.id)
+     .update(req.body)
+	.returning(['foldername', 'userid'])
+	.then(result => {
+		return res.status(201).json(result);
+	})
+	.catch(error => { 
+		console.log(error);
+		return res.status(500).json({message: "Internal server error"});  
+	});
+})
+
+
+
+// // FOLDER DELETE ENDPOINT
+
+app.delete('/folders/:id', (req, res) => {
+	knex.del()
+    .where('folderid',req.params.id)
+    .from('folders')
+   .then(result => {
+     return res.status(202).send('delete was successful');
+   })
+	 .catch(err => {
+   	 console.log(err);
+		 return res.status(500).json({message: "Internal server error"}); 
+   });
+});
+
+
+
+
+
+
 
 // Serve the built client
 app.use(express.static(path.resolve(__dirname, '../client/build')));
@@ -53,3 +231,4 @@ if (require.main === module) {
 module.exports = {
     app, runServer, closeServer
 };
+
